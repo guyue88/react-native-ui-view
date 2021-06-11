90083e0aa28b70e600da16c53d2410968637c740
luoqiang <qiang.luo@shopee.com>
2021-06-11 15:01:53 +0800

    feat: entry

--- index.ts
+++
@@ -1,4 +0,0 @@
-export { default as TabBar, TabItem } from './src/components/TabBar';
-export { default as Navbar } from './src/components/Navbar';
-
-export default {};
---
+++ src/index.ts
@@ -0,0 +1,5 @@
+export { default as TabBar } from './components/TabBar';
+export type { TabItem } from './components/TabBar';
+export { default as Navbar } from './components/Navbar';
+
+export default {};
