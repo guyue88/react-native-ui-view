043458742ea2ad97d880ab42d446182df8992d4a
luoqiang <qiang.luo@shopee.com>
2021-06-23 14:25:38 +0800

    feat: add icon

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 export { COLORS, FONT_WEIGHT } from './components/Styles/theme';
 export { default as Badge } from './components/Badge';
 export { default as Navbar } from './components/Navbar';
+export { default as Icon } from './components/Icon';
 export { default as TabBar } from './components/TabBar';
 export { default as TabItem } from './components/TabBar/item';
